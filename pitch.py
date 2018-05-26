54c8524652ec9d07f612f39be99d62d93e8d3278
kaiubi <kaiubiferreira@gmail.com>
2018-05-26 16:55:22 -0300

    REAPER

--- pitch.py
+++ pitch.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pyreaper
 from scipy.signal import find_peaks, medfilt
 from scipy import signal, fftpack
 
 class Pitch:
     def __init__(self, audio):
         self.audio = audio
-        #autocorr = self.autocorrelation()
-        plt.plot(self.autocorrelation())
-        plt.plot(self.cepstrum())
-        plt.show()
+        autocorr = self.autocorrelation()
+        reaper = self.reaper()
+        x = np.linspace(0, self.audio.duration, len(autocorr))
+        plt.plot(x, autocorr)
+        x = np.linspace(0, self.audio.duration, len(reaper))
+        plt.plot(x, reaper)
 
+        plt.show()
 
     def autocorrelation(self):
         autocorr_list = [np.correlate(window, window, 'full') for window in self.audio.windows]
@@ -29,27 +33,10 @@ class Pitch:
 
         return f0_list
 
-    def cepstrum(self):
-        f0_list = []
-        for win in self.audio.windows:
-            fft_result = np.log(np.square(fftpack.rfft(win * signal.hann(self.audio.window_size))))
-            ceps = np.square(fftpack.ifft(fft_result).real)
-            ceps = ceps[20:int(len(ceps) / 2)]
-            f0_list.append(self.audio.rate / (np.argmax(ceps)))
+    def reaper(self):
+        intWaveForm = np.ndarray.flatten(self.audio.waveform * 32000).astype('int16')
+        pm_times, pm, f0_times, f0_list, corr = pyreaper.reaper(intWaveForm, self.audio.rate)
 
-        #plt.plot(f0_list)
-        #plt.show()
         return f0_list
-        #print(np.argmax(ceps))
-        #print(self.audio.rate / np.argmax(ceps))
 
-        # plt.subplot(3, 1, 1)
-        # plt.plot(self.audio.windows[35])
-        # plt.subplot(3, 1, 2)
-        # plt.plot(fft_result)
-        # plt.subplot(3, 1, 3)
-        # plt.plot(ceps)
-        #
-        # #plt.subplot(3, 1, 3)
-        # #plt.plot(cepstrum)
-        # plt.show()
+
